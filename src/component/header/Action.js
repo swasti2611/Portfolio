0411ca78d2ce9a2211f9a2ca30951af8f36a4973
manishloginn <manishv7714@gmail.com>
2024-08-03 22:55:23 +0530

    third commit

--- src/component/header/Action.js
+++ src/component/header/Action.js
@@ -9,7 +9,7 @@ function Action() {
   return (
     <div>
       <div className='action'>
-        <a className='btn' href='Resume' download>Download Resume </a>
+        <a className='btn' href={Resume} download>Download Resume </a>
         <a className='btn btn-primary' href="#contact">Contact me!</a>
       </div>
     </div>
